d3319bc7a4b887c9f674e6330aaa185385724c79
lucdon <lucdonders1@gmail.com>
2017-03-23 11:08:15 +0100

    j

--- js/quickfit.js
+++ js/quickfit.js
@@ -1,15 +1,4 @@
-$('.hover').textfill({
-    minFontPixels: 2,
-    maxFontPixels: 100,
-    innerTag: 'span',
-    widthOnly: false,
-    explicitWidth: null,
-    explicitHeight: null,
-    debug: false,
-    changeLineHeight: false
-});
-
-$(window).resize(function(){
+$(window).on("load, resize", function() {
     $('.hover').textfill({
         minFontPixels: 2,
         maxFontPixels: 100,
