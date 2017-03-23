30d51bba792af66b298b38143dcbf5c8c60c0188
lucdon <lucdonders1@gmail.com>
2017-03-23 11:06:49 +0100

    fix

--- js/quickfit.js
+++ js/quickfit.js
@@ -8,3 +8,16 @@ $('.hover').textfill({
     debug: false,
     changeLineHeight: false
 });
+
+$(window).resize(function(){
+    $('.hover').textfill({
+        minFontPixels: 2,
+        maxFontPixels: 100,
+        innerTag: 'span',
+        widthOnly: false,
+        explicitWidth: null,
+        explicitHeight: null,
+        debug: false,
+        changeLineHeight: false
+    });
+});
