b255efdf92051c85cb5f26051d157b7fb1ffd93f
lucdon <lucdonders1@gmail.com>
2017-03-30 15:14:17 +0200

    homepage finished

--- js/mobile.js
+++ js/mobile.js
@@ -5,6 +5,7 @@ $(window).on('load, resize', function mobileViewUpdate() {
     var page2 = $(".page2");
     var page3 = $(".page3");
     var page4 = $(".page4");
+    var footer = $(".footer");
 
     $(".hover").each(function(){
         if (viewportWidth < 767) {
@@ -97,8 +98,16 @@ $(window).on('load, resize', function mobileViewUpdate() {
             }
         } else {
             $(this).css("height", largeSize);
-            page2.css("height", "120%");
-            page3.css("top", "220%");
+            if(viewportHeight < 700 && viewportHeight >= 520){
+                page2.css("height", "150%");
+                page3.css("top", "250%");
+            } else if(viewportHeight < 520){
+                page2.css("height", "250%");
+                page3.css("top", "350%");
+            } else {
+                page2.css("height", "120%");
+                page3.css("top", "220%");
+            }
         }
     });
 
@@ -109,55 +118,76 @@ $(window).on('load, resize', function mobileViewUpdate() {
                 page3.css("height", "130%");
                 page4.css("top", "350%");
                 page4.css("height", "130%");
+                footer.css("top", "480%");
             } else if(viewportHeight < 1030 && viewportHeight >= 950){
                 page3.css("height", "140%");
                 page4.css("top", "360%");
                 page4.css("height", "140%");
+                footer.css("top", "500%");
             } else if(viewportHeight < 950 && viewportHeight >= 890){
                 page3.css("height", "150%");
                 page4.css("top", "370%");
                 page4.css("height", "150%");
+                footer.css("top", "520%");
             } else if(viewportHeight < 890 && viewportHeight >= 785){
                 page3.css("height", "170%");
                 page4.css("top", "410%");
                 page4.css("height", "170%");
+                footer.css("top", "590%");
             } else if(viewportHeight < 750 && viewportHeight >= 700){
                 page3.css("height", "190%");
                 page4.css("top", "450%");
                 page4.css("height", "190%");
+                footer.css("top", "640%");
             } else if(viewportHeight < 700 && viewportHeight >= 600){
                 page3.css("height", "220%");
                 page4.css("top", "490%");
                 page4.css("height", "220%");
+                footer.css("top", "710%");
             } else if(viewportHeight < 600 && viewportHeight >= 530){
                 page3.css("height", "250%");
                 page4.css("top", "550%");
                 page4.css("height", "250%");
+                footer.css("top", "800%");
             } else if(viewportHeight < 530 && viewportHeight >= 440){
                 page3.css("height", "300%");
                 page4.css("top", "650%");
                 page4.css("height", "300%");
+                footer.css("top", "950%");
             } else if(viewportHeight < 440){
                 page3.css("height", "400%");
                 page4.css("top", "800%");
                 page4.css("height", "400%");
+                footer.css("top", "1200%");
             } else {
                 page3.css("height", "120%");
                 page4.css("top", "340%");
                 page4.css("height", "120%");
+                footer.css("top", "460%");
             }
         } else {
             $(this).css("height", largeSize);
-            if(viewportHeight < 760){
-                page3.css("height", "125%");
-                page4.css("top", "345%");
-                page4.css("height", "125%");
+            if(viewportHeight < 760 && viewportHeight >= 700){
+                page3.css("height", "130%");
+                page4.css("top", "350%");
+                page4.css("height", "130%");
+                footer.css("top", "480%");
+            } else if(viewportHeight < 700 && viewportHeight >= 520) {
+                page3.css("height", "180%");
+                page4.css("top", "430%");
+                page4.css("height", "180%");
+                footer.css("top", "610%");
+            } else if(viewportHeight < 520) {
+                page3.css("height", "280%");
+                page4.css("top", "630%");
+                page4.css("height", "280%");
+                footer.css("top", "910%");
             } else {
                 page3.css("height", "120%");
                 page4.css("top", "340%");
                 page4.css("height", "120%");
+                footer.css("top", "460%");
             }
-            //TODODOODODOODOD: width > 767 and height < 700 todooddoododod
         }
     });
 });
